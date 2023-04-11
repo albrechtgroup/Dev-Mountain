56096854f0ed7d7ad06ac64b004fc11ef989bad3
Andy Albrecht <aalbrecht33733@gmail.com>
2023-04-11 12:50:56 -0400

    first commit, changes comming

---
+++ 2_week/oopDemo/app.js
@@ -0,0 +1,21 @@
+let person = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 43
+}
+
+console.log(person.age); // 43
+person.isTacky = true;
+console.log(person)
+
+console.log(person.isTacky); // true
+
+// Destructure:
+let meal = {
+    appetizer: 'Mozz sticks',
+    entree: 'Pizza',
+    dessert: 'Cheesecake'
+}
+
+
+
