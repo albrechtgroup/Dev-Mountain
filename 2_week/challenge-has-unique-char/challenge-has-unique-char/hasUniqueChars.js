233ebc7ea9bcf949aae96da261e7d90bb2da0a75
Andy Albrecht <aalbrecht33733@gmail.com>
2023-04-12 11:09:44 -0400

    hasUniqueChar.js exercise complete

--- 2_week/challenge-has-unique-char/challenge-has-unique-char/hasUniqueChars.js
+++ 2_week/challenge-has-unique-char/challenge-has-unique-char/hasUniqueChars.js
@@ -1,1 +1,15 @@
-// Write your code below
+// Basic Version:
+function hasUniqueCharacters(str) {
+  for (let i = 0; i < str.length; i++) {
+    for (let j = i + 1; j < str.length; j++) {
+      if (str[i] === str[j]) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+console.log(hasUniqueCharacters("hello")); // false
+console.log(hasUniqueCharacters("world")); // true
+console.log(hasUniqueCharacters("javascript")); // true
