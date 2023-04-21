4e3a3f6bfe8f67539b27ac834e33e57d4f9ade38
Andy Albrecht <aalbrecht33733@gmail.com>
2023-04-21 09:48:04 -0400

    Js-requests-demo updated code and comments

--- 3_week/js-requests-demo/main.js
+++ 3_week/js-requests-demo/main.js
@@ -1,4 +1,41 @@
-console.log('connected')
+// console.log('connected')
+
+// const getAllBtn = document.querySelector('#all')
+// const charBtns = document.querySelectorAll('.char-btns')
+// const ageForm = document.querySelector('#age-form')
+// const ageInput = document.querySelector('#age-input')
+// const createForm = document.querySelector('#create-form')
+// const newFirstInput = document.querySelector('#first')
+// const newLastInput = document.querySelector('#last')
+// const newGenderDropDown = document.querySelector('select')
+// const newAgeInput = document.querySelector('#age')
+// const newLikesText = document.querySelector('textarea')
+// const charContainer = document.querySelector('section')
+
+// // const baseURL = 
+
+// function createCharacterCard(char) {
+//   let charCard = document.createElement('div')
+//   charCard.innerHTML = `<h3>${char.firstName} ${char.lastName}</h3>
+//   <p>gender: ${char.gender} | age: ${char.age}</p>
+//   <h4>Likes</h4>
+//   <ul>
+//     <li>${char.likes[0]}</li>
+//     <li>${char.likes[1]}</li>
+//     <li>${char.likes[2]}</li>
+//   </ul>`
+
+//   charContainer.appendChild(charCard)
+// }
+
+// function clearCharacters() {
+//   charContainer.innerHTML = ``
+// }
+
+
+
+/////////////////////////////////////////////////
+/// *===> DJ Code <===* ///
 
 const getAllBtn = document.querySelector('#all')
 const charBtns = document.querySelectorAll('.char-btns')
@@ -11,8 +48,11 @@ const newGenderDropDown = document.querySelector('select')
 const newAgeInput = document.querySelector('#age')
 const newLikesText = document.querySelector('textarea')
 const charContainer = document.querySelector('section')
+const editForm = document.querySelector('#edit-form')
+const nameEditInput = document.querySelector('#name-change')
+const newNameEditInput = document.querySelector('#new-name-input')
 
-// const baseURL = 
+const baseURL = `http://localhost:4000`
 
 function createCharacterCard(char) {
   let charCard = document.createElement('div')
@@ -31,3 +71,79 @@ function createCharacterCard(char) {
 function clearCharacters() {
   charContainer.innerHTML = ``
 }
+
+const getAllChars = () => {
+  axios.get(`${baseURL}/characters`)
+  .then(res => {
+    console.log(res.data)
+    clearCharacters()
+    res.data.map(char => createCharacterCard(char))
+  })
+  .catch(err => console.log(err))
+}
+
+const getSingleChar = (event) => {
+  console.log(event.target.id)
+  axios.get(`${baseURL}/character/${event.target.id}`)
+  .then(response => {
+    console.log(response)
+    clearCharacters()
+    createCharacterCard(response.data)
+  })
+  .catch(err => console.log(err))
+}
+
+const getOldChars = (event) => {
+  event.preventDefault()
+
+  axios.get(`${baseURL}/character?age=${ageInput.value}`)
+  .then(res => {
+    console.log(res.data)
+    clearCharacters()
+    res.data.map(char => createCharacterCard(char))
+  })
+  .catch(err => console.log(err))
+}
+
+const addNewChar = (e) => {
+  e.preventDefault()
+
+  // console.log(newLikesText.value)
+  let newLikes = newLikesText.value.split(',')
+
+  const body = {
+    firstName: newFirstInput.value,
+    lastName: newLastInput.value,
+    gender:newGenderDropDown.value,
+    age:newAgeInput.value,
+    likes:newLikes
+  }
+
+  axios.post(`${baseURL}/character`, body)
+  .then(res => {
+    clearCharacters()
+    res.data.map(char => createCharacterCard(char))
+  })
+  .catch(err => console.log(err))
+}
+
+const editChar = (event) => {
+  event.preventDefault()
+
+  axios.put(`${baseURL}/character/${nameEditInput.value}/${newNameEditInput.value}`)
+  .then(res => {
+    console.log(res.data)
+    clearCharacters()
+    res.data.map(char => createCharacterCard(char))
+  })
+  .catch(err => console.log(err))
+}
+
+for(let i = 0;i < charBtns.length;i++){
+  charBtns[i].addEventListener('click',getSingleChar)
+}
+
+editForm.addEventListener('submit',editChar)
+createForm.addEventListener('submit',addNewChar)
+ageForm.addEventListener('submit',getOldChars)
+getAllBtn.addEventListener('click',getAllChars)
