7cbc135c20613cfb3611087c38c922aa83d5914a
Andy Albrecht <aalbrecht33733@gmail.com>
2023-04-25 10:53:40 -0400

    Updated code and comments

---
+++ 4_week/movieSearchDB/app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const request = require("request");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+    res.render("search");
+});
+
+app.get("/results", (req, res) => {
+    var query = req.query.search;
+    var url = "http://www.omdbapi.com/?s=" + query + "&apikey=thewdb";
+    request(url, (error, response, body) => {
+       if(!error && response.statusCode == 200){
+           var data = JSON.parse(body);
+           res.render("results", {data: data});
+       } 
+    });
+});
+
+
+
+//localhost:3000
+app.listen(3000, function() { 
+    console.log('=== Server listening on port 3000');
+    console.log('=== Movie App has Started!'); 
+  });
