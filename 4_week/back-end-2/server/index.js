6e8fe17752967527cadcbbc2ffd9df24c814284a
Andy Albrecht <aalbrecht33733@gmail.com>
2023-04-25 17:21:43 -0400

    Back-end-2 mission complete

---
+++ 4_week/back-end-2/server/index.js
@@ -0,0 +1,34 @@
+// const express = require('express');
+// const cors = require('cors');
+// const app = express();
+
+// app.use(express.json());
+// app.use(cors());
+
+
+
+// app.listen(4000, () => console.log('Port Running on 4000'));
+
+
+///////////////////////////////////////////////
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+const {
+    getHouses,
+    deleteHouse, 
+    createHouse, 
+    updateHouse
+} = require('./controller')
+
+app.get(`/api/houses`, getHouses)
+app.delete(`/api/houses/:id`, deleteHouse)
+app.post(`/api/houses`, createHouse)
+app.put(`/api/houses/:id`, updateHouse)
+
+app.listen(4004, () => console.log(`running on 4004`))
